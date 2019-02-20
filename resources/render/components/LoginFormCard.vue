e8316910f34eaecdc9f2966a2b583365163cb7e6
Zendy Lim <zendy.me@gmail.com>
2019-02-21 00:54:06 +0900

    Split Login view to components

---
+++ resources/render/components/LoginFormCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card class="elevation-12">
+    <login-form-header text="Login" color="primary"/>
+    <login-form-card-form/>
+    <login-form-card-sns-buttons/>
+    <v-divider inset/>
+    <login-form-card-register-link text="Don't have an account?" url="/signup" urlText="Sign Up"/>
+  </v-card>
+</template>
+
+<script>
+  import LoginFormHeader from './LoginFormCardHeader';
+  import LoginFormCardForm from './LoginFormCardForm';
+  import LoginFormCardFacebookButton from './LoginFormCardFacebookButton';
+  import LoginFormCardGoogleButton from './LoginFormCardGoogleButton';
+  import LoginFormCardTwitterButton from './LoginFormCardTwitterButton';
+  import LoginFormCardSnsButtons from './LoginFormCardSNSButtons';
+  import LoginFormCardRegisterLink from './LoginFormCardRegisterLink';
+
+  export default {
+    name: 'login-form',
+    components: {
+      LoginFormCardRegisterLink,
+      LoginFormCardSnsButtons,
+      LoginFormCardTwitterButton,
+      LoginFormCardGoogleButton,
+      LoginFormCardFacebookButton,
+      LoginFormCardForm,
+      LoginFormHeader,
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ resources/render/components/LoginFormCardForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <v-card-text>
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation>
+        <v-text-field
+          prepend-icon="person"
+          name="login"
+          label="E-mail"
+          type="text"
+          required
+          v-model="email"
+          :rules="emailRules"
+        ></v-text-field>
+        <v-text-field
+          id="password"
+          prepend-icon="lock"
+          name="password"
+          label="Passwords"
+          type="password"
+          v-model="password"
+          :rules="passwordRules"
+          required
+        ></v-text-field>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn :disabled="!valid"
+             color="success"
+             @click="validate"
+             large block>
+        Login
+      </v-btn>
+    </v-card-actions>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login-form-card-form',
+    data() {
+      return {
+        valid: true,
+        email: '',
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+/.test(v) || 'E-mail must be valid'
+        ],
+        password: '',
+        passwordRules: [
+          v => !!v || 'Password is required'
+        ]
+      };
+    },
+    methods: {
+      validate() {
+        if (this.$refs.form.validate()) {
+          this.snackbar = true;
+        }
+      }
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
