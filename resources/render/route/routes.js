62db1915d2a97f81227bb17ad28a804602a76bfb
Zendy Lim <zendy.me@gmail.com>
2019-02-20 18:50:56 +0900

    Create basic routing for templates

--- resources/render/route/routes.js
+++ resources/render/route/routes.js
@@ -1,4 +1,8 @@
 import Home from '../views/Home';
+import Login from '../views/Login';
+import Profile from '../views/Profile';
+import Community from '../views/Community';
+import SignUp from '../views/SignUp';
 
 export default {
   mode: 'history',
@@ -6,6 +10,22 @@ export default {
     {
       path: '/',
       component: Home
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/community',
+      component: Community
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/signup',
+      component: SignUp
     }
   ]
 };
