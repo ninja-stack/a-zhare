d4fa5728af9771b2bdaaa257f6396a906abe6bee
hafendy <harfendybit@gmail.com>
2019-02-21 00:48:59 +0900

    Add profile view

--- resources/render/views/Profile.vue
+++ resources/render/views/Profile.vue
@@ -1,11 +1,95 @@
 <template>
   <v-container fluid>
-    <!--Put Here-->
+    <v-navigation-drawer
+      permanent
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-card>
+              <v-form>
+                <v-textarea
+                >
+                </v-textarea>
+              </v-form>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn flat class="primary">Submit</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+          <v-layout row wrap pa-2></v-layout>
+          <v-flex xs12 sm12>
+            <v-card>
+              <v-card-title primary-title>
+                <div>
+                  <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+                  <div> {{ card_text }} </div>
+                </div>
+              </v-card-title>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+                aspect-ratio="2.75"
+              ></v-img>
+
+              <v-card-actions>
+                <v-list-tile class="grow">
+                  <v-layout
+                    align-center
+                    justify-end
+                  >
+                    <v-icon class="mr-1">mdi-heart</v-icon>
+                    <span class="subheading mr-2">256</span>
+                    <span class="mr-1">·</span>
+                    <v-icon class="mr-1">mdi-share-variant</v-icon>
+                    <span class="subheading">45</span>
+                  </v-layout>
+                </v-list-tile>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
   </v-container>
 </template>
 
 <script>
-  export default {};
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
 </script>
 
 <style scoped lang="scss">
