0402fe1fe7de4bf729c74a3b3f2ce16cedc569e2
viinkelvin <viinkelvin@gmail.com>
2019-02-25 10:00:02 +0900

    Update Chat Components
    - Add chat detail chat panel

--- resources/render/views/Chat.vue
+++ resources/render/views/Chat.vue
@@ -4,7 +4,7 @@
     <v-content>
       <v-layout row wrap>
 				<chat-list />
-				<chat-detail />
+				<chat-detail name="Kelvin"/>
       </v-layout>
     </v-content>
   </v-container>
@@ -19,92 +19,8 @@
 			NavigationMenu,
 			ChatList,
 			ChatDetail,
-    },
-    data: () => ({
-			drawer: null,
-			chatlists: [
-				{ header: 'Today' },
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-					title: 'Brunch this weekend?',
-					subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
-				},
-				{ divider: true, inset: true },
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-					title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-					subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-				},
-				{ divider: true, inset: true },
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-					title: 'Oui oui',
-					subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-				},
-				{ divider: true, inset: true },
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-					title: 'Birthday gift',
-					subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
-				},
-				{ divider: true, inset: true },
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-					title: 'Recipe to try',
-					subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
-				},
-				{ divider: true, inset: true },
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-					title: 'Recipe to try 2',
-					subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
-				},
-				{ divider: true, inset: true },
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-					title: 'Recipe to try 3',
-					subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
-				}
-			],
-			chats: [
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-					subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
-					type: true,
-				},
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-					subtitle: "Wish I could come, but I'm out of town this weekend.",
-					type: false,
-				},
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-					subtitle: "Do you have Paris recommendations? Have you ever been?",
-					type: true,
-				},
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-					subtitle: "Have any ideas about what we should get Heidi for her birthday?",
-					type: false,
-				},
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-					subtitle: "We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
-					type: true,
-				},	
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-					subtitle: "We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
-					type: false,
-				},
-				{
-					avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-					subtitle: "We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
-					type: true,
-				}
-			]
-      }),
-    props: {
+		},
+		props: {
       source: String
     }
   }
