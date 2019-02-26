088473e84ab770e0b163a4c6b8457edf28f0dcc9
viinkelvin <viinkelvin@gmail.com>
2019-02-26 21:54:57 +0900

    Update News Feed Profile
    - Update news feed profile
    - Make profile components

---
+++ resources/render/components/ProfileRequestTabContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card flat>
+    <request-post-card/>
+    <request-post-card/>
+  </v-card>
+</template>
+
+<script>
+import RequestPostCard from '../components/RequestPostCard';
+export default {
+  name: 'profile-request-tab-content',
+  components: {
+    RequestPostCard
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
